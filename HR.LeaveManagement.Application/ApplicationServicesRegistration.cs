11c11d481820915fd635bddff50e78799fc1f79d
osoji <osoji@simplexsystem.com>
2023-05-25 09:39:04 +0100

    First commit

---
+++ HR.LeaveManagement.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,25 @@
+﻿using HR.LeaveManagement.Application.Profies;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace HR.LeaveManagement.Application
+{
+    public static class ApplicationServicesRegistration
+    {
+        public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services) 
+        { 
+            //The issue with this is that for every mapping you have, you will have to repeat this profile
+            //services.AddAutoMapper(typeof(MappingProfile));
+
+            //Why this work for every mapping profile
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
---
+++ HR.LeaveManagement.Domain/Common/BaseDomainEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Domain.Common
+{
+    //I make this class an abstract class for it to be inheritable and to also able to instantiate
+    public abstract class BaseDomainEntity
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+    }
+}
